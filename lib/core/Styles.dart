a8f6d6d5d8bd723c391ee1a33c6606822d7082ae
m7md3bbas <mhmdabass330@gmail.com>
2023-08-29 20:49:31 +0300

    finish OnBoarding Page

---
+++ lib/core/Styles.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+abstract class styles {
+  static const style = TextStyle();
+}
--- lib/widgets/CustomOnBoarding.dart
+++ lib/widgets/CustomOnBoarding.dart
@@ -1,16 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:restaurant_app/core/Styles.dart';
 
 class CustomOnBoarding extends StatelessWidget {
-  CustomOnBoarding({super.key});
+  CustomOnBoarding({this.subtitle, this.title, this.imageUrl, super.key});
   Color backGroundPageColor = const Color(0xffFFFFFF);
-
+  String? imageUrl;
+  String? title;
+  String? subtitle;
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(color: backGroundPageColor),
-      child: const Center(
+      child: Center(
         child: Column(
-          children: [],
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Spacer(
+              flex: 2,
+            ),
+            Image.asset(imageUrl!),
+            const SizedBox(
+              height: 77.27,
+            ),
+            Text(
+              title!,
+              style: styles.style.copyWith(
+                  fontSize: 24,
+                  color: const Color(0xff1F2937),
+                  fontWeight: FontWeight.w600),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Text(
+              subtitle!,
+              style: styles.style
+                  .copyWith(fontSize: 12, color: const Color(0xff4B5563)),
+              textAlign: TextAlign.center,
+            ),
+            const Spacer()
+          ],
         ),
       ),
     );
