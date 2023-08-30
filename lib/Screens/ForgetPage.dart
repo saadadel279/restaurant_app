03fea4182c570baad7a96bdb2a665562b2fd3f00
m7md3bbas <mhmdabass330@gmail.com>
2023-08-30 17:29:31 +0300

    create Forget Password

---
+++ lib/Screens/ForgetPage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ForgetPasswordPage extends StatelessWidget {
+  const ForgetPasswordPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [Text('Forget Password')],
+      ),
+    );
+  }
+}
